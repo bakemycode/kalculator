61381ca19254de598d0b9ea2383e63e1530c8a2f
bakemycode <34391558+bakemycode@users.noreply.github.com>
2018-08-22 14:35:12 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,52 +1,21 @@
 ﻿using System;
 using MySql.Data.MySqlClient;
 
-
 namespace kalculrator3
 
 {
     class Program
     {
-        /*
-        private static void SelectUsingReader()
-        {
-            var direct = "Server=localhost;Database=Kalculator3;Uid=root;Pwd=theBigBang!23;";
-
-            using (MySqlConnection conn = new MySqlConnection(direct))
-            {
-                conn.Open();
-
-                string sql = "SELECT * FROM FoodList";
-
-                MySqlCommand cmd = new MySqlCommand(sql, conn);
-                MySqlDataReader rdr = cmd.ExecuteReader();
-
-                while (rdr.Read())
-                {
-                    Console.WriteLine("NAME : {0}",rdr["NAME"]);
-                    Console.WriteLine("CALORIES : {0}", rdr["CALORIES"]);
-                    Console.WriteLine("CARB : {0}", rdr["CARB"]);
-                    Console.WriteLine("PROTEIN : {0}", rdr["PROTEIN"]);
-                    Console.WriteLine("FAT : {0}", rdr["FAT"]);
-                    Console.WriteLine();
-
-                }
-                rdr.Close();
-            }
-        }
-*/
         static void Main(string[] args)
         {
-            var strConn = "Server=localhost;Database=Kalculator3;Uid=root;Pwd=theBigBang!23;";
-            MySqlConnection connection = new MySqlConnection(strConn);
-        
-
-            connection.Open();
-            Console.WriteLine(connection.State);
+            // SelectUsingReader()
+            var reading = new SelectUsingReader();
+            //if a parameter is input, it will show the details of it, otherwise all of items.
+            reading.SelectUsingReader_();
 
 
-            var reading = new SelectUsing_Reader();
-            reading.SelectUsingReader();
+
+
         }
     }
 
